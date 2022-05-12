8fdf8709e57ea1b627b6deac1ad6e8dcb28f44fc
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-12 16:05:28 +0000

    alex

---
+++ estRepetitivas/EjerciciosR.java
@@ -0,0 +1,19 @@
+package estRepetitivas;
+
+/**
+ * EjerciciosR
+ */
+public class EjerciciosR {
+
+    public static void imprimirWhileN1_10() {
+        int numTnit=1;
+        while (numTnit<=20) {
+            System.out.println(numTnit);
+            numTnit=numTnit+1;
+        }
+    }
+
+    public static void main(String[] args) {
+        imprimirWhileN1_10();
+    }
+}
