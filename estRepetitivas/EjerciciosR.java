1e3673820d5f9699244f6e8b62d3ed88c779d94b
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-12 16:45:11 +0000

    alex

--- estRepetitivas/EjerciciosR.java
+++ estRepetitivas/EjerciciosR.java
@@ -20,9 +20,17 @@ public class EjerciciosR {
         } while (numTnit<=20);
     }
 
+    public static void imprimirForN1_20() {
+        for (int numTnit = 1; numTnit <= 20; numTnit++) {
+            System.out.println(numTnit);
+        }
+            
+    }
+       
 
     public static void main(String[] args) {
        System.out.println("Imprime While"); imprimirWhileN1_20();
        System.out.println("Imprime Do While"); imprimirDowhileN1_20();
+       System.out.println("Imprime for"); imprimirForN1_20();
     }
 }
