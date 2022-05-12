6914c6db10d50f27dd56c04637058846d57af044
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-12 16:26:26 +0000

    alex

--- estRepetitivas/EjerciciosR.java
+++ estRepetitivas/EjerciciosR.java
@@ -5,15 +5,24 @@ package estRepetitivas;
  */
 public class EjerciciosR {
 
-    public static void imprimirWhileN1_10() {
-        int numTnit=1;
+    public static void imprimirWhileN1_20() {
+        int numTnit=21;
         while (numTnit<=20) {
             System.out.println(numTnit);
             numTnit=numTnit+1;
         }
     }
+    public static void imprimirDowhileN1_20() {
+        int numTnit=21;
+        do {
+            System.out.println(numTnit);
+            numTnit++;
+        } while (numTnit<=20);
+    }
+
 
     public static void main(String[] args) {
-        imprimirWhileN1_10();
+       System.out.println("Imprime While"); imprimirWhileN1_20();
+       System.out.println("Imprime Do While"); imprimirDowhileN1_20();
     }
 }
