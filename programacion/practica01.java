27c8c05f4721ae2c5c388588943ccf000c0563a0
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-16 13:01:12 +0000

    alex

---
+++ programacion/practica01.java
@@ -0,0 +1,12 @@
+package programacion;
+
+/**
+ * practica01
+ */
+public class practica01 {
+
+    public static void main(String[]args) {
+        System.out.println("hola mundo");
+        
+    }
+}
