1267b81b47f4653112da27efd0f9bca75c5c3ba1
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-16 13:26:31 +0000

    alex

---
+++ programacion/practica02.java
@@ -0,0 +1,14 @@
+package programacion;
+
+public class practica02 {
+    public static void main(String args[]) {
+        
+        int numero_uno = 5;
+        int numero_dos = 2;
+        int resultado = 0;
+
+        resultado = numero_uno + numero_dos;
+        System.out.println("El valor de tu suma es: " + resultado);
+        
+    }
+}
