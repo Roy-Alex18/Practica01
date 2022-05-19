07341e7131d7da527cf53d6020c11df3c9131999
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-19 17:58:03 +0000

    alex

--- estRepetitivas/ejercicio4_10.java
+++ estRepetitivas/ejercicio4_10.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class ejercicio4_10 {
     static Scanner leerT = new Scanner(System.in);
 
-    public static int factorialN(int numero) {
-        int resultado=1;
+    public static long factorialN(int numero) {
+        long resultado=1;
         if (numero>1) {
             for (int i = 1; i <= numero; i++) {
                 resultado=resultado*i;
@@ -19,8 +19,11 @@ public class ejercicio4_10 {
         
     }
 
-
     public static void main(String args[]) {
-        System.out.println(factorialN(5));
+        System.out.println("Ingrese un numero: ");
+        int num=leerT.nextInt();
+        for (int i = 0; i < num; i++) {
+            System.out.println("Factorial de "+ i+ "es: " + factorialN(i));
+        }
     }
 }
