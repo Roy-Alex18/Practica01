4dedadeb8ec10da1e7225c68e6ea961be014162b
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-19 17:22:26 +0000

    Alex

---
+++ estRepetitivas/ejercicio4_10.java
@@ -0,0 +1,26 @@
+package estRepetitivas;
+
+import java.util.Scanner;
+
+/**
+ * ejercicio4_10
+ */
+public class ejercicio4_10 {
+    static Scanner leerT = new Scanner(System.in);
+
+    public static int factorialN(int numero) {
+        int resultado=1;
+        if (numero>1) {
+            for (int i = 1; i <= numero; i++) {
+                resultado=resultado*i;
+            }
+        }
+        return resultado;
+        
+    }
+
+
+    public static void main(String args[]) {
+        System.out.println(factorialN(5));
+    }
+}
