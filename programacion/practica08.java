fdb95cf71d2ab2a6c5d8e27bfb0eb3d02021bb5a
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-22 20:02:09 +0000

    alex

---
+++ programacion/practica08.java
@@ -0,0 +1,15 @@
+package programacion;
+import java.util.Scanner;
+
+public class practica08 {
+
+    public static void main(String args[]) {
+
+        Scanner in =new Scanner(System.in);
+        String nombre = "";
+        int numuno = 0, numdos = 0, resultado = 0;
+
+        System.out.println("Cual es tu nombre ");
+        nombre = in.nextLine();
+    }
+}
