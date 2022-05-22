44afd459e5292f435772d632bce162310488dd59
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-22 19:26:27 +0000

    alex

--- programacion/practica06.java
+++ programacion/practica06.java
@@ -3,5 +3,17 @@ package programacion;
 public class practica06 {
     public static void main(String args[]) {
         
+        int matematicas = 5;
+        int biologuia = 8;
+        int quimica = 7;
+        int promedio = 0;
+
+        promedio =(matematicas+biologuia+quimica)/3;
+
+        if(promedio>=6){
+            System.out.println("El alumno aprobo: "+ promedio);
+        }else if(promedio >= 6){
+            System.out.println("El alumno desaprobo: "+ promedio);
+        }
     }
 }
