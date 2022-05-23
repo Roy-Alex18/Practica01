0f73e98f7d5e5716391b318c015bfa4885bc4e90
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-23 22:13:45 +0000

    alex

--- programacion/practica13.java
+++ programacion/practica13.java
@@ -1,1 +1,11 @@
-public class practica13
+public class practica13 {
+    public static void main(String args[]) {
+        
+        int i = 1000;
+
+        do{
+            System.out.println(i + ", ");
+            i -= 200;
+        } while( i >= 0);
+    }
+}
