d2f255c24d9e433d0103937fb1e7e8402645dd30
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-25 00:00:34 +0000

    alex

---
+++ tarea/ejercicio4_1.java
@@ -0,0 +1,13 @@
+package tarea;
+
+public class ejercicio4_1 {
+    
+    public static void main(String[] args) {
+        int salario = 1500;
+        double r = 0;
+        for(int i = 0; i <= 6; i++){
+            r = salario+(salario*0.10)*i;
+            System.out.println("El salario al cabo de 6 años es: "+ r);
+        }
+    }
+}
