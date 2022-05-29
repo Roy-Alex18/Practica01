e87c092102f989bb2004e63fff26238cacab661a
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-29 16:51:11 +0000

    roy alex gorveña suarez

--- tarea/ejercicio4_2.java
+++ tarea/ejercicio4_2.java
@@ -1,6 +1,20 @@
 package tarea;
 
+import java.util.Scanner;
+
 public class ejercicio4_2 {
     
+    public static void main(String args[]){
+
+        int n, tipo, pago;
+        double total;
+
+        System.out.println("Ingrese el total de hamburguesas: ");
+        Scanner n = new Scanner(System.in);
+        
+        do{
+
+        }while(n>1)
+    }
 }
     
--- tarea/ejercicio4_3.java
+++ tarea/ejercicio4_3.java
@@ -1,5 +1,32 @@
 package tarea;
 
+import java.util.Scanner;
+
 public class ejercicio4_3 {
-    
+    public static void main(String args[]) {
+        
+        int n, numero, a = 0, b = 0, c = 0;
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("determina numeros menores, mayores e iguales a cero: ");
+
+        System.out.println("ingresa el valor: ");
+        n = leer.nextInt();
+
+        for(int i = 0; i < n; i++){
+            System.out.println("ingresa el numero: ");
+            numero = leer.nextInt();
+
+            if(numero > 0){
+                a = a+1;
+
+            } else if(numero < 0){
+                b = b+1;
+            }else {
+                c = c+1;
+            }
+        }
+        System.out.println("Numeros mayores a cero: " + a + " Numeros menores a cero: "+ b + " Numeros iguales a cero:" + c);
+
+    }
 }
