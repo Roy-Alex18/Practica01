d07ff7fc468a999435e54dee8ec9f47fbc72c496
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-05-29 19:42:24 +0000

    roy alex gorveña suarez

--- tarea/ejercicio4_2.java
+++ tarea/ejercicio4_2.java
@@ -3,18 +3,47 @@ package tarea;
 import java.util.Scanner;
 
 public class ejercicio4_2 {
-    
-    public static void main(String args[]){
+
+    public static void main(String[] args){
 
         int n, tipo, pago;
-        double total;
+        double total = 0;
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Hanburguesas sencillas, dobles o triples");
 
         System.out.println("Ingrese el total de hamburguesas: ");
-        Scanner n = new Scanner(System.in);
-        
-        do{
+        n = leer.nextInt();
+        while(n > 0){
+            System.out.println("Elige una haburguesa: ");
+            System.out.println("1 = sencilla $10");
+            System.out.println("2 = doble $12");
+            System.out.println("3 = triple $14");
+            tipo = leer.nextInt();
+            if(tipo >= 1 && tipo <= 3){
+                if(tipo == 1){
+                    total = total + 10;
+                }else if(tipo == 1){
+                    total = total + 12;
+                }else {
+                    total = total + 14;
+                }
+                n = n - 1;
+            }
+            System.out.println("Ingrese el numero correcto: ");
+        }
 
-        }while(n>1)
+        System.out.println("Ingresa el tipo de pago:");
+        System.out.println("1 = efectivo");
+        System.out.println("2 = tarjeta");
+        pago = leer.nextInt();
+        if(pago == 1 || pago == 2) {
+            if(pago == 2){
+                total = total + (total * 0.05);
+                System.out.println("Se aplico un cargo del 5%");
+            }
+            System.out.println("El total a apagar es: " + total);
+        }
+        System.out.println("Lo siento no te podemos cobrar.");  
     }
 }
-    
---
+++ tarea/ejercicio4_5.java
@@ -0,0 +1,39 @@
+package tarea;
+
+import java.util.Scanner;
+
+public class ejercicio4_5 {
+    public static void main(String[] args) {
+        
+        int n, color,verde,blanco,rojo;
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de focos: ");
+        n =leer.nextInt();
+
+        verde = 0;
+        blanco = 0;
+        rojo = 0;
+        while(n > 0){
+            System.out.println("Selecciona un color");
+            System.out.println("1 = verde");
+            System.out.println("2 =blanco");
+            System.out.println("3 = rojo");
+            color = leer.nextInt();
+            if(color >= 1 && color <= 3){
+                if(color == 1){
+                    verde = verde +1;
+                }else if(color == 2){
+                    blanco = blanco + 1;
+                }else {
+                    rojo = rojo + 1;
+                }
+                n = n - 1;
+            }
+            System.out.println("Ingresa un color correcto");  
+        }
+        System.out.println("El total de focos verdes es: "+ verde);
+        System.out.println("El total de focos blancos es: "+ blanco);
+        System.out.println("El total de focos rojos es: "+ rojo);
+    }
+}
