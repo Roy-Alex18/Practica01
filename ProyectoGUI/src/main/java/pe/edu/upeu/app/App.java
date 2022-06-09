2dac147356bbb0d4914e80b6e21a7f2d40459bb4
Roy-Alex18 <roy.gorvena@upeu.edu.pe>
2022-06-09 13:11:21 -0500

    alex

---
+++ ProyectoGUI/src/main/java/pe/edu/upeu/app/App.java
@@ -0,0 +1,16 @@
+package pe.edu.upeu.app;
+
+import java.net.MalformedURLException;
+
+import pe.edu.upeu.gui.MainGUI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new MainGUI();
+    }
+}
--- programacion/practica01.java
+++ programacion/practica01.java
@@ -5,8 +5,8 @@ package programacion;
  */
 public class practica01 {
 
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         System.out.println("hola mundo");
-        
+
     }
 }
